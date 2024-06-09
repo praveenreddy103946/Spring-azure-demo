6531bd0cf3b871548df43003b2c7b76c25ce73ea
Praveen Redamalla <praveen.redamalla@acsicorp.com>
2024-06-09 19:03:39 +0530

    second commit

--- src/main/java/com/neverstop/neverstop/NeverstopApplication.java
+++ src/main/java/com/neverstop/neverstop/NeverstopApplication.java
@@ -4,13 +4,15 @@ import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-@RestController("/azure")
+@RestController
+@RequestMapping("/praveen")
 public class NeverstopApplication {
 
 	public static void main(String[] args) {
